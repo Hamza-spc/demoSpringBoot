96ed7b1ca195137358ca4305dbdbfe6feecbc6d8
Hamza <hamza.2simou@gmail.com>
2026-02-24 17:49:24 +0000

    first rest api of app

--- src/main/java/com/practiceSpring/demo/HelloWorldController.java
+++ src/main/java/com/practiceSpring/demo/HelloWorldController.java
@@ -3,11 +3,14 @@ package com.practiceSpring.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //
+/* tells spring this class is gonna deal with http requests and return http responses
+without it spring will think that you wanna return an HTML page
+*/
+@RestController
 public class HelloWorldController {
 
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "/hello") //tells spring when an HTTP GET request arrive on /hello to execute the method
     public String helloWorld(){
-        return "Hello World";
+        return "Hello World!";
     }
 }
